6387c32e5cdf764666198c46887cdfbc0e4a49cc
YushMC <mujushaxb10@gmail.com>
2024-12-17 19:28:12 -0600

    version 0.0.6

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/app/", // Cambia esto según tu ruta de despliegue
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Esto configura el alias @ para apuntar a la carpeta src
